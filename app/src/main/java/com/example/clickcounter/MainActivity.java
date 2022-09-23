41e68327f181c2df755eefbc6026f7479e547cc5
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-23 08:59:03 +0200

    Complete task

--- app/src/main/java/com/example/clickcounter/MainActivity.java
+++ app/src/main/java/com/example/clickcounter/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.clickcounter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,12 +13,44 @@ public class MainActivity extends AppCompatActivity {
     private Button plusz;
     private Button minusz;
 
+    static int szamlalo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        szam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                szamlalo = 0;
+                szam.setText("0");
+                szam.setTextColor(Color.rgb(0,0,255));
+            }
+        });
+        plusz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                szamlalo ++;
+                String pluszEgy = String.format("%s",szamlalo);
+                szam.setText(pluszEgy);
+                if (szamlalo > 0){
+                    szam.setTextColor(Color.GREEN);
+                }
+            }
+        });
+        minusz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                szamlalo --;
+                String minuszEgy = String.format("%s",szamlalo);
+                szam.setText(minuszEgy);
+                if (szamlalo < 0){
+                    szam.setTextColor(Color.RED);
+                }
 
+            }
+        });
     }
     private void init(){
         szam = findViewById(R.id.szam);
